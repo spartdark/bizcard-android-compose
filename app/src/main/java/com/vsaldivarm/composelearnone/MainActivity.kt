42993dbf316780560ca62213a4ee55cb4dd7d965
Vladimir Saldivar <vsaldivarm@gmail.com>
2023-05-14 00:18:00 -0600

    adding LazyColumn

--- app/src/main/java/com/vsaldivarm/composelearnone/MainActivity.kt
+++ app/src/main/java/com/vsaldivarm/composelearnone/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.vsaldivarm.composelearnone
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,9 +17,12 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
@@ -25,6 +30,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -33,6 +40,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.vsaldivarm.composelearnone.ui.theme.ComposeLearnOneTheme
+import kotlin.math.round
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,10 +61,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun createBizCard() {
+    val buttonClickState = remember {
+        mutableStateOf(false)
+    }
     Surface(
         modifier = Modifier
             .fillMaxWidth()
-            .fillMaxHeight()) {
+            .fillMaxHeight()
+    ) {
         Card(
             modifier = Modifier
                 .width(200.dp)
@@ -74,8 +86,60 @@ fun createBizCard() {
                 CreateImageProfile()
                 Divider()
                 DataText()
+                Button(onClick = {
+                    Log.i("VSM", "Button taped")
+                    //hide and show the list
+                    buttonClickState.value = !buttonClickState.value
+
+                }) {
+                    //pass parameters
+                    Text(
+                        text = "Portafolio",
+                        style = MaterialTheme.typography.titleMedium
+                    )
+
+                }
+                if(buttonClickState.value){
+                    Content()
+                }else{
+                    Box(){
+
+                    }
+                }
             }
 
+        }
+    }
+}
+
+
+@Composable
+fun Content() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(5.dp)
+    ) {
+        Surface(
+            modifier = Modifier
+                .padding(3.dp)
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+            border = BorderStroke(width = 2.dp, color = Color.LightGray)
+        ) {
+Portafolio (data= listOf("Project 1","Project 2", "Project 3","Project 4","Project 5", "Project 6"))
+        }
+    }
+}
+
+@Composable
+fun Portafolio(data: List<String>) {
+    LazyColumn{
+        items(data){
+            item: String ->
+            Text(text = item)
         }
     }
 }
